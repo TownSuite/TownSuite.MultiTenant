2b9b62ee03762702f5415a6ef4a8cc1d0879c579
Peter Gill <peter.gill@townsuite.com>
2023-06-29 14:24:56 -0230

    Tenants test

---
+++ TownSuite.MultiTenant.Tests/Tenant_Tests.cs
@@ -0,0 +1,42 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace TownSuite.MultiTenant.Tests;
+
+public class Tenant_Tests
+{
+    [Test]
+    public async Task Appsettings_Test()
+    {
+    }
+
+    [Test]
+    public void Clone_Test()
+    {
+        var t = new Tenant("abc");
+        t.Connections.Add("app1", "conn1");
+        t.Connections.Add("app2", "conn2");
+        
+        var clone = t.Clone() as Tenant;
+        Assert.That(clone, Is.Not.Null);
+        Assert.That(clone.UniqueId, Is.EqualTo(t.UniqueId));
+        Assert.That(clone.Connections.Count, Is.EqualTo(t.Connections.Count));
+        Assert.That(clone.Connections["app1"], Is.EqualTo(t.Connections["app1"]));
+        Assert.That(clone.Connections["app2"], Is.EqualTo(t.Connections["app2"]));
+    }
+    
+    [Test]
+    public void Equals_Test()
+    {
+        var t1 = new Tenant("abc");
+        t1.Connections.Add("app1", "conn1");
+        t1.Connections.Add("app2", "conn2");
+        
+        var t2 = new Tenant("abc");
+        t2.Connections.Add("app1", "conn1");
+        t2.Connections.Add("app2", "conn2");
+        
+        Assert.That(t1.Equals(t2), Is.True);
+    }
+}
